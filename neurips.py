e2d9d0ad5efe022e4eccfdea5fd942690fc8a75e
Matt L Laporte <matt.l.laporte@gmail.com>
2018-11-27 01:28:53 -0500

    Much more documentation. Option to exclude posters from the calendars; they are by far the largest category, so this is much faster. Fixed logging of events added to calendar. Made a couple of functions to repeat myself less.

--- neurips.py
+++ neurips.py
@@ -1,7 +1,30 @@
+"""Create Google calendar events from the NeurIPS 2018 website event list.
+
+Scrapes event data from https://nips.cc using `requests` and BeautifulSoup.
+Creates and populates a separate calendar for each event type. Metadata
+includes the full description/abstract prepended with the author/speaker names,
+as well as a link to the event details page (or the proceedings page, when
+possible).
+
+If the `USE_HISTORY` constant is `True`, the IDs of processed events will be
+logged, and IDs encountered more than once will be skipped. In a single session
+this should not happen (i.e. no repeat IDs in the entire list of events on the
+website), but this allows the creation of the calendar to take place over
+multiple sessions, if necessary.
+
+Before running, visit https://developers.google.com/calendar/quickstart/python
+to activate the Google Calendar API, and place the `credentials.json` file in
+the same directory as this script.
+
+The Google Calendar API authentication code is derived from the guides linked
+above.
+"""
+
+
 import datetime
 import re
 
-import bs4
+from bs4 import BeautifulSoup
 from dateutil.parser import parse
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -10,79 +33,111 @@ from oauth2client import file, client, tools
 import pytz
 import requests
 
+# exclude posters from the calendar
+# Note: much faster when `True` & poster calendar is basically unusable anyway
+EXCLUDE_POSTERS = True
+POSTER_EVENT_TYPE = 'Poster'
+
 # if True, keep record of processed events, and don't repeat insertions
 USE_HISTORY = True
-HISTORY_FILE = 'processed_event_name_hashes.log'
+HISTORY_FILE = 'processed_event_ids.log'
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/calendar'
+# If you change this, delete token.json.
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/calendar'
 
-# URLs for the schedule and proceedings
+# BeautifulSoup things
+HTML_PARSER = 'html.parser'
+
+# URLs of pages with data to be scraped
 SCHEDULE_URL = 'https://nips.cc/Conferences/2018/Schedule'
 EVENT_URL = 'https://nips.cc/Conferences/2018/Schedule?showEvent='
 PAPERS_URL = 'https://papers.nips.cc'
-PROC_URL = 'https://papers.nips.cc/book/advances-in-neural-information-processing-systems-31-2018'
+PROC_URL = ('https://papers.nips.cc/book/'
+            + 'advances-in-neural-information-processing-systems-31-2018')
 
-
-# EVENT_TYPES = ['Tutorial', 'Break', 'Poster', 'Oral', 'Spotlight',
-#                'Demonstration', 'Invited Talk', 'Workshop', 'Talk']
-
+# locale
 TIMEZONE = 'America/Montreal'  # this is an alias for America/Toronto
 LOCATION = '1001 Jean Paul Riopelle Pl, Montreal, QC H2Z 1H5'
 
+find_all_url(url, **kwargs):
+    """Shorthand for BeautifulSoup `find_all` on a url, by `requests`."""
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, HTML_PARSER)
+    found = soup.find_all(**kwargs)
+    return found
+
+datetime_strs_to_rfc3339(strs, tz=timezone):
+    """Parse partial datetime strings, output RFC 3339 timestamp.
+
+    Some partial datetime strings are: "Mon", "Dec", "3" (or "3rd"),
+    "12:00:00".
+    """
+    dt = parse(' '.join(strs))
+    dt_loc = timezone.localize(dt)
+    rfc = dt_loc.isoformat('T')
+    return rfc
+
 def main():
+    # Google authentication
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        flow = client.flow_from_clientsecrets('credentials.json', OAUTH_SCOPE)
         creds = tools.run_flow(flow, store)
     service = build('calendar', 'v3', http=creds.authorize(Http()))
 
     # get links to all papers
-    proc_html = requests.get(PROC_URL).text
-    proc_soup = bs4.BeautifulSoup(proc_html, 'html.parser')
-    proc_tags = proc_soup.find_all(href=re.compile('/paper/'))
+    proc_tags = find_all_url(PROC_URL, href=re.compile('/paper/'))
     papers = dict()
     for tag in proc_tags:
         papers[tag.text] = PAPERS_URL + tag.attrs['href']
 
-    # get all events (as HTML div tags)
-    sched_html = requests.get(SCHEDULE_URL).text
-    sched_soup = bs4.BeautifulSoup(sched_html, 'html.parser')
-    event_divs = sched_soup.find_all(id=re.compile('maincard_'))
+    # get all div tags corresponding to events
+    event_tags = find_all_url(SCHEDULE_URL, id=re.compile('maincard_'))
 
-    # timezone object for formatting timestamps
+    # timezone object for localizing timestamps
     timezone = pytz.timezone(TIMEZONE)
 
+    # get metadata for existing calendars on account
     calendars = service.calendarList().list().execute()['items']
     calendar_ids_by_name = {c['summary']: c['id'] for c in calendars}
-
+    # make sure a calendar exists for each event type
     event_types = [d.find(class_="pull-right maincardHeader maincardType").text
-                   for d in event_divs]
-    for event_type in set(event_types):
-        if not event_type in calendar_ids_by_name:
-            calendar = dict(
-                summary=event_type,
-                timeZone=TIMEZONE,
-                location=LOCATION,
-            )
-            service.calendars().insert(body=calendar).execute()
+                   for d in event_tags]
+    new_calendar_names = [event_type in set(event_types)
+                          if not event_type in calendar_ids_by_name]
+    if any(new_calendar_names):
+        calendar = dict(
+            summary=event_type,
+            timeZone=TIMEZONE,
+            location=LOCATION,
+        )
+        service.calendars().insert(body=calendar).execute()
 
-    calendars = service.calendarList().list().execute()['items']
-    calendar_ids_by_name = {c['summary']: c['id'] for c in calendars}
+        # need the IDs generated for the new calendars
+        calendars = service.calendarList().list().execute()['items']
+        calendar_ids_by_name = {c['summary']: c['id'] for c in calendars}
 
+    # try to load history if toggled
     if USE_HISTORY:
-        with open(HISTORY_FILE, 'r') as history_file:
-            processed_events = [int(line.rstrip())
-                                for line in history_file.readlines()]
-        history_file = open(HISTORY_FILE, 'a')
+        try:
+            with open(HISTORY_FILE, 'r') as history_file:
+                processed_events = [int(line.rstrip())
+                                    for line in history_file.readlines()]
+        except FileNotFoundError:
+            processed_events = []
 
-    for div, event_type in zip(event_divs, event_types):
-        event_id = div.attrs['id'].split('_')[1]
-        if event_id in processed_events:
-            continue
-        processed_events.append(event_id)
-        print(event_id, file=history_file)
+    # process the list of events
+    for div, event_type in zip(event_tags, event_types):
+        event_id = int(div.attrs['id'].split('_')[1])
+        if USE_HISTORY:
+            if event_id in processed_events:
+                continue
+
+        if EXCLUDE_POSTERS:
+            if re.search(POSTER_EVENT_TYPE, event_type):
+                # it's a poster and we're skipping posters
+                continue
 
         # extract information from tags
         event_name = div.find(class_="maincardBody").text
@@ -99,20 +154,19 @@ def main():
             # AM/PM same for start and end; add to start string for parsing
             start_strs += [end_strs[-1]]
         end_strs = start_strs[:-2] + end_strs
-        start_time = parse(' '.join(start_strs))
-        start_time = timezone.localize(start_time).isoformat('T')
-        end_time = parse(' '.join(end_strs))
-        end_time = timezone.localize(end_time).isoformat('T')
+        start_time = datetime_strs_to_rfc3339(start_strs)
+        end_time = datetime_strs_to_rfc3339(end_strs)
 
         # get event description from details page
-        event_url = EVENT_URL + event_id
-        event_doc = requests.get(event_url).text
-        event_soup = bs4.BeautifulSoup(event_doc, 'html.parser')
-        event_description = event_soup.find(class_='abstractContainer').text
+        event_description = find_all_url(EVENT_URL + str(event_id)
+                                         class_='abstractContainer')[0].text
 
+        # add list of authors/speakers at the start of the description
         event_description = (', '.join(event_speakers) + '\n\n'
                              + event_description)
 
+        # if the event title is in the proceedings, link to there. else details
+        # NOTE: I'm not sure this does anything. I don't see any attachments.
         try:
             link_url = papers[event_name]
             file_url = link_url + '.pdf'
@@ -120,6 +174,7 @@ def main():
             link_url = event_url
             file_url = ""
 
+        # information needed to add the event to the calendar
         event_spec = dict(
             summary=event_name,
             location=loc,
@@ -145,6 +200,12 @@ def main():
         g_event = service.events().insert(calendarId=calendar_id,
                                           body=event_spec)
         g_event.execute()
+
+        # keep track, if ordered
+        if USE_HISTORY:
+            processed_events.append(event_id)
+            with open(HISTORY_FILE, 'a') as history_file:
+                print(event_id, file=history_file)
 
 if __name__ == '__main__':
     main()
